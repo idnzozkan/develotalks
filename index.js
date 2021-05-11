2aff23d5f2687a57cf9802aec2edb41c1fb3ae21
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-05-11 14:24:10 +0300

    Create a basic db with fs module

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const Tags = require("./enums/Tags");
 const Interests = require("./enums/Interests");
 const Languages = require("./enums/Languages");
 
+const colors = require("colors");
+
+const db = require("./database");
+
 const dennis = new User(
   1,
   "Deniz",
@@ -12,19 +16,6 @@ const dennis = new User(
   ["twitter.com/account", "github.com/account", "linkedin.com/in/account"],
   [Interests.FRONTEND, Interests.BACKEND, Interests.DEVOPS],
   [Languages.ENGLISH, Languages.TURKISH]
-);
-
-const dennisRoom = dennis.createRoom(
-  "Cool room",
-  "This is the description of our cool room",
-  Languages.ENGLISH,
-  2,
-  true,
-  true,
-  true,
-  true,
-  true,
-  [Tags.JAVASCRIPT, Tags.PAIR_PROGRAMMING, Tags.WEB_DEVELOPMENT]
 );
 
 const john = new User(
@@ -49,9 +40,47 @@ const kristina = new User(
   [Languages.RUSSIAN, Languages.ENGLISH]
 );
 
+const dennisRoom = dennis.createRoom(
+  "Cool room",
+  "This is the description of our cool room",
+  Languages.ENGLISH,
+  2,
+  true,
+  true,
+  true,
+  true,
+  true,
+  [Tags.JAVASCRIPT, Tags.PAIR_PROGRAMMING, Tags.WEB_DEVELOPMENT]
+);
+
 john.joinRoom(dennisRoom);
-kristina.joinRoom(dennisRoom);
 dennis.acceptWaitingUser();
-dennis.stopSession();
-john.stopSession();
-kristina.joinRoom(dennisRoom);
+
+// db.save("createdRooms", [dennisRoom]);
+// db.save("users", [dennis, john]);
+// db.insert("users", kristina);
+// db.remove("users", 2);
+const foundUser = db.findByName("users", "John Doe");
+
+const createdRooms = db.load("createdRooms");
+const users = db.load("users");
+
+if (foundUser.onlineAtRoom) {
+  console.log(
+    colors.blue(`${foundUser.name}`),
+    "online in a room with room ID",
+    colors.bgRed.white(`${foundUser.onlineAtRoom.shareableLink}`)
+  );
+} else {
+  console.log(
+    colors.red(`${colors.bgRed.white(`${foundUser.name}`)} is not in a room`)
+  );
+}
+
+// createdRooms.forEach((room) => {
+//   console.log("Room:", room.title);
+// });
+
+// users.forEach((user) => {
+//   console.log("User:", user.name);
+// });
