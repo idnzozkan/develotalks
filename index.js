299c78ab2a3f9842401f633c34702c77c37e5bf6
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-05-25 13:23:27 +0300

    Add routes as middlewares

--- index.js
+++ index.js
@@ -1,28 +1,17 @@
 const express = require("express")
-const { stringify } = require("flatted")
-const { createdRoomsDatabase, usersDatabase } = require("./database")
+const bodyParser = require("body-parser")
+const indexRouter = require("./routes/index")
+const userRouter = require("./routes/user")
+const roomRouter = require("./routes/room")
+
 const app = express()
+app.use(bodyParser.json())
 
 app.set("view engine", "pug")
 
-app.get("/u/:id", async (req, res) => {
-  const user = await usersDatabase.find(req.params.id)
-  if (!user) return res.status(404).send("404 - Cannot find user")
-
-  res.render("user", { user })
-})
-
-app.get("/r/:id", async (req, res) => {
-  const room = await createdRoomsDatabase.find(req.params.id)
-  if (!room) return res.status(404).send("404 - Cannot find room")
-
-  res.render("room", { room })
-})
-
-app.get("/", async (req, res) => {
-  const rooms = await createdRoomsDatabase.load()
-  res.render("index", { rooms })
-})
+app.use("/u", userRouter)
+app.use("/r", roomRouter)
+app.use("/", indexRouter)
 
 app.listen(3000, () => {
   console.log("started listening")
