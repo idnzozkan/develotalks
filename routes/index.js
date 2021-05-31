00d16e2651736215707ec56b4e812616c149c5cc
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-05-31 23:45:30 +0300

    Refactor database to services

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
-const { createdRoomsDatabase } = require("../database")
+const { createdRoomsService } = require("../services")
 const router = require("express").Router()
 
 router.get("/", async (req, res) => {
-  const rooms = await createdRoomsDatabase.load()
+  const rooms = await createdRoomsService.load()
 
   res.render("index", { rooms })
 })
