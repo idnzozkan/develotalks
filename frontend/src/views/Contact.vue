960f40c15281438ed9b022c72e960273e2bda512
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-07-25 20:36:47 +0300

    Modify dummy views and components

---
+++ frontend/src/views/Contact.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: 'Contact',
+  props: {
+    email: String
+  }
+}
+</script>
+
+<template lang="pug">
+  div
+    h1 Get in touch
+    span {{ email }}
+</template>
+
+<style scoped lang="scss">
+  h1 {
+    color: red;
+  }
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,10 +1,3 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
@@ -16,3 +9,9 @@ export default {
   }
 }
 </script>
+
+<template lang="pug">
+  .home
+    img(alt="Vue logo" src="../assets/logo.png")
+    HelloWorld(msg="Welcome to Your Vue.js App")
+</template>
