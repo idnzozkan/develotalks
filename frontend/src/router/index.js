e0e40b6c1f5866d998dfe84a63041ab81afcf8f3
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-08-05 23:55:34 +0300

    Remove unused views from the router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import Contact from '../views/Contact.vue'
 
 Vue.use(VueRouter)
 
@@ -10,23 +9,6 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/contact',
-    name: 'Contact',
-    component: Contact,
-    props: {
-      email: 'ibrahimdenizozkan@gmail.com'
-    }
   }
 ]
 
