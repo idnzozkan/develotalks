7cb3e7757a934da63b5879e4e5d18ac7305e22d5
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-11-14 20:33:03 +0300

    Move RoomCard from Home component to a separate component file

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,38 +1,21 @@
 <script>
-import { mapActions } from 'vuex'
-import RoomCard from '@/components/content/RoomCard.vue'
+import RoomList from '@/components/rooms/RoomList.vue'
 
 export default {
   name: 'Home',
   components: {
-    RoomCard
-  },
-  data () {
-    return {
-      rooms: [],
-      isLoading: true
-    }
-  },
-  methods: {
-    ...mapActions(['fetchRooms'])
-  },
-  async mounted () {
-    this.rooms = await this.fetchRooms()
-    this.isLoading = false
+    RoomList
   }
 }
 </script>
 
 <template lang="pug">
-  .home
-    p(v-if="isLoading") Please wait...
-    div(v-else v-for="room in rooms")
-      RoomCard(:title="room.title" :owner="room.owner" :participants="room.participants" :language="room.roomLanguage" :tags="room.roomTags" :maxParticipants="room.maxParticipants" :isPrivate="room.isPrivate")
+  .container
+    RoomList
 </template>
 
 <style lang="scss" scoped>
-.home {
-  display: flex;
-  padding: 10rem;
-}
+  .container {
+    padding: 4rem 8.87500rem;
+  }
 </style>
