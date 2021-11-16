062d0ccd7683745239dd658c76be562d2297388a
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-11-16 23:43:32 +0300

    Import related sidebar components to the main Sidebar component

--- frontend/src/components/sidebar/Sidebar.vue
+++ frontend/src/components/sidebar/Sidebar.vue
@@ -1,86 +1,41 @@
 <script>
+import Header from './Header.vue'
+import Profile from './Profile.vue'
+import CreateRoomButton from './create-room/create-room-button'
+import Menu from './Menu.vue'
+import FriendsList from './friends-list'
+import Footer from './Footer.vue'
+
 export default {
-  name: 'Sidebar'
+  name: 'Sidebar',
+  components: {
+    Header,
+    Profile,
+    CreateRoomButton,
+    Menu,
+    FriendsList,
+    Footer
+  }
 }
 </script>
 
 <template lang="pug">
   .sidebar-container
-    .header
-      span.title DeveloTalks
-      .hamburger
-        .line
-        .line
-
-    .profile
-      .user-avatar-frame
-        img.user-avatar(src="https://i.ibb.co/PwQ7Dxv/profile.jpg")
-      span.user-display-name Deniz Ozkan
-      span.username @deniz
-      .user-stats
-        .total-stars
-          span 1.4K
-          span Stars
-        .total-followers
-          span 1.2K
-          span Followers
-        .total-following
-          span 100
-          span Following
-
-    .create-room-btn-wrapper
-      btn.create-room-btn Create Room
-
-    .sidebar-menu
-      ul
-        li
-          router-link(to="/") Home
-      ul
-        li
-          router-link(to="/messages") Messages
-      ul
-        li
-          router-link(to="/settings") Settings
-
-    .friends-list
-      span.section-title Friends
-        span.total-friends (20)
-
-      .friend-container
-        img.friend-avatar(src="https://avatarmaker.net/images/3.png")
-        span.friend-display-name Angela Lawson
-        .online-status
-
-      .friend-container
-        img.friend-avatar(src="https://avatarmaker.net/images/3.png")
-        span.friend-display-name Steven Williams
-        .online-status
-
-      .friend-container
-        img.friend-avatar(src="https://avatarmaker.net/images/3.png")
-        span.friend-display-name Victoria I. Rice
-        .online-status
-
-      .show-more
-        a.show-more-btn(href="#")
-        span Show more
-
-      .sidebar-footer
-        p.footer-description
-        | DeveloTalks is an open-source project. Feel free to contribute on
-        a(href="https://www.github.com/idnzozkan/develotalks") GitHub
-        | .
-        p.copyright-text
-        | DeveloTalks © 2021
-        a(href="https://www.twitter.com/idnzozkan") Deniz Ozkan
-
+    Header
+    Profile
+    CreateRoomButton
+    Menu
+    FriendsList
+    Footer
 </template>
 
 <style lang="scss" scoped>
 .sidebar-container {
-  width: 20vw;
-  height: 100%;
-  background: rgba(179, 158, 177, 0.7);
+  position: relative;
+  padding: 2.4375rem;
+  width: 21.562rem;
+  height: 100vh;
+  overflow: hidden;
+  border-right: 1px solid #2C353D;
 }
-
 </style>
