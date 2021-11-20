66a41b86239f89919681656d5043f52b44c285c2
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-11-20 21:51:37 +0300

    Fix fake data for eslint rules

--- frontend/src/@fake-db/rooms.js
+++ frontend/src/@fake-db/rooms.js
@@ -1,18 +1,18 @@
-const getRooms = () => [
+const getRooms = async () => [
   {
-    _id: "60b526e0ec34b4f603d4b64f",
+    _id: '60b526e0ec34b4f603d4b64f',
     participants: [
       {
-        _id: "60b4ce37b66493b3b07ee3e5",
+        _id: '60b4ce37b66493b3b07ee3e5',
         socialLinks: [
-          "https://www.codewars.com/"
+          'https://www.codewars.com/'
         ],
         interests: [
-          "Frontend"
+          'Frontend'
         ],
         spokenLangs: [
-          "German",
-          "English"
+          'German',
+          'English'
         ],
         friends: [],
         following: [],
@@ -20,33 +20,59 @@ const getRooms = () => [
         messages: [],
         starCount: 0,
         starredUsers: [],
-        name: "Eren Jaeger",
-        username: "eren.jaeger",
-        profilePhoto: "https://static.wikia.nocookie.net/shingekinokyojin/images/a/a1/Eren_Jaeger_%28Anime%29_character_image.png",
-        userBio: "I'm Eren Jaeger, a Jr. Frontend Developer",
+        name: 'Eren Jaeger',
+        username: 'eren.jaeger',
+        profilePhoto: 'https://static.wikia.nocookie.net/shingekinokyojin/images/a/a1/Eren_Jaeger_%28Anime%29_character_image.png',
+        userBio: 'I am Eren Jaeger, a Jr. Frontend Developer',
         activeRoom: null,
-        createdRoom: "60b526e0ec34b4f603d4b64f",
+        createdRoom: '60b526e0ec34b4f603d4b64f',
+        waitingRoom: null
+      },
+      {
+        _id: '60b4ce37b66493b3b07ee3e5',
+        socialLinks: [
+          'https://www.codewars.com/'
+        ],
+        interests: [
+          'Frontend'
+        ],
+        spokenLangs: [
+          'German',
+          'English'
+        ],
+        friends: [],
+        following: [],
+        followers: [],
+        messages: [],
+        starCount: 0,
+        starredUsers: [],
+        name: 'Eren Jaeger',
+        username: 'eren.jaeger',
+        profilePhoto: 'https://static.wikia.nocookie.net/shingekinokyojin/images/a/a1/Eren_Jaeger_%28Anime%29_character_image.png',
+        userBio: 'I am Eren Jaeger, a Jr. Frontend Developer',
+        activeRoom: null,
+        createdRoom: '60b526e0ec34b4f603d4b64f',
         waitingRoom: null
       }
     ],
     kickedPeople: [],
     waitingPeople: [],
     roomTags: [
-      "#frontend",
-      "#webdevelopment",
-      "#javascript"
+      '#frontend',
+      '#webdevelopment',
+      '#javascript'
     ],
     owner: {
-      _id: "60b4ce37b66493b3b07ee3e5",
+      _id: '60b4ce37b66493b3b07ee3e5',
       socialLinks: [
-        "https://www.codewars.com/"
+        'https://www.codewars.com/'
       ],
       interests: [
-        "Frontend"
+        'Frontend'
       ],
       spokenLangs: [
-        "German",
-        "English"
+        'German',
+        'English'
       ],
       friends: [],
       following: [],
@@ -54,39 +80,39 @@ const getRooms = () => [
       messages: [],
       starCount: 0,
       starredUsers: [],
-      name: "Eren Jaeger",
-      username: "eren.jaeger",
-      profilePhoto: "https://static.wikia.nocookie.net/shingekinokyojin/images/a/a1/Eren_Jaeger_%28Anime%29_character_image.png",
-      userBio: "I'm Eren Jaeger, a Jr. Frontend Developer",
+      name: 'Eren Jaeger',
+      username: 'eren.jaeger',
+      profilePhoto: 'https://thispersondoesnotexist.com/image',
+      userBio: 'I am Eren Jaeger, a Jr. Frontend Developer',
       activeRoom: null,
-      createdRoom: "60b526e0ec34b4f603d4b64f",
+      createdRoom: '60b526e0ec34b4f603d4b64f',
       waitingRoom: null
     },
-    title: "Looking for some Front-End Developers",
-    description: "talking about future of the frontend engineering",
-    roomLanguage: "Turkish",
+    title: 'Looking for some Front-End Developers',
+    description: 'talking about future of the frontend engineering',
+    roomLanguage: 'Turkish',
     maxParticipants: 2,
     canUseMic: true,
     canUseWebcam: false,
     canShareScreen: true,
     canTypeToChatBox: true,
     isPrivate: true,
-    createdAt: "2021-05-31T18:11:44.410Z"
+    createdAt: '2021-05-31T18:11:44.410Z'
   },
   {
-    _id: "60b526e0ec34b4f603d4b66f",
+    _id: '60b526e0ec34b4f603d4b66f',
     participants: [
       {
-        _id: "60b4ce37b66493b3b07ee3e7",
+        _id: '60b4ce37b66493b3b07ee3e7',
         socialLinks: [
-          "https://www.github.com/"
+          'https://www.github.com/'
         ],
         interests: [
-          "Backend"
+          'Backend'
         ],
         spokenLangs: [
-          "Spanish",
-          "English"
+          'Spanish',
+          'English'
         ],
         friends: [],
         following: [],
@@ -94,32 +120,32 @@ const getRooms = () => [
         messages: [],
         starCount: 0,
         starredUsers: [],
-        name: "John Doe",
-        username: "john.doe",
-        profilePhoto: "https://avatarmaker.net/images/1.png",
-        userBio: "I'm John Doe, a Sr. Backend Developer",
+        name: 'John Doe',
+        username: 'john.doe',
+        profilePhoto: 'https://avatarmaker.net/images/1.png',
+        userBio: 'I am John Doe, a Sr. Backend Developer',
         activeRoom: null,
-        createdRoom: "60b526e0ec34b4f603d4b64f",
+        createdRoom: '60b526e0ec34b4f603d4b64f',
         waitingRoom: null
       }
     ],
     kickedPeople: [],
     waitingPeople: [],
     roomTags: [
-      "#nodejs",
-      "#backend"
+      '#nodejs',
+      '#backend'
     ],
     owner: {
-      _id: "60b4ce37b66493b3b07ee3e7",
+      _id: '60b4ce37b66493b3b07ee3e7',
       socialLinks: [
-        "https://www.github.com/"
+        'https://www.github.com/'
       ],
       interests: [
-        "Backend"
+        'Backend'
       ],
       spokenLangs: [
-        "Spanish",
-        "English"
+        'Spanish',
+        'English'
       ],
       friends: [],
       following: [],
@@ -127,39 +153,39 @@ const getRooms = () => [
       messages: [],
       starCount: 0,
       starredUsers: [],
-      name: "John Doe",
-      username: "john.doe",
-      profilePhoto: "https://avatarmaker.net/images/1.png",
-      userBio: "I'm John Doe, a Sr. Backend Developer",
+      name: 'John Doe',
+      username: 'john.doe',
+      profilePhoto: 'https://avatarmaker.net/images/1.png',
+      userBio: 'I am John Doe, a Sr. Backend Developer',
       activeRoom: null,
-      createdRoom: "60b526e0ec34b4f603d4b64f",
+      createdRoom: '60b526e0ec34b4f603d4b64f',
       waitingRoom: null
     },
-    title: "Title of the room!!",
-    description: "lorem ipsum dolor sit amet",
-    roomLanguage: "English",
+    title: 'Title of the room!!',
+    description: 'lorem ipsum dolor sit amet',
+    roomLanguage: 'English',
     maxParticipants: 4,
     canUseMic: true,
     canUseWebcam: true,
     canShareScreen: true,
     canTypeToChatBox: true,
     isPrivate: false,
-    createdAt: "2021-05-31T19:11:44.410Z"
+    createdAt: '2021-05-31T19:11:44.410Z'
   },
   {
-    _id: "60b526e0ec34b4f603d4b61f",
+    _id: '60b526e0ec34b4f603d4b61f',
     participants: [
       {
-        _id: "60b4ce37b66493b3b07ee3e6",
+        _id: '60b4ce37b66493b3b07ee3e6',
         socialLinks: [
-          "https://www.github.com/"
+          'https://www.github.com/'
         ],
         interests: [
-          "Backend"
+          'Backend'
         ],
         spokenLangs: [
-          "Spanish",
-          "English"
+          'Spanish',
+          'English'
         ],
         friends: [],
         following: [],
@@ -167,32 +193,32 @@ const getRooms = () => [
         messages: [],
         starCount: 0,
         starredUsers: [],
-        name: "John Doe",
-        username: "john.doe",
-        profilePhoto: "https://avatarmaker.net/images/1.png",
-        userBio: "I'm John Doe, a Sr. Backend Developer",
+        name: 'John Doe',
+        username: 'john.doe',
+        profilePhoto: 'https://avatarmaker.net/images/1.png',
+        userBio: 'I am John Doe, a Sr. Backend Developer',
         activeRoom: null,
-        createdRoom: "60b526e0ec34b4f603d4b64f",
+        createdRoom: '60b526e0ec34b4f603d4b64f',
         waitingRoom: null
       }
     ],
     kickedPeople: [],
     waitingPeople: [],
     roomTags: [
-      "#nodejs",
-      "#backend"
+      '#nodejs',
+      '#backend'
     ],
     owner: {
-      _id: "60b4ce37b66493b3b07ee3e6",
+      _id: '60b4ce37b66493b3b07ee3e6',
       socialLinks: [
-        "https://www.github.com/"
+        'https://www.github.com/'
       ],
       interests: [
-        "Backend"
+        'Backend'
       ],
       spokenLangs: [
-        "Spanish",
-        "English"
+        'Spanish',
+        'English'
       ],
       friends: [],
       following: [],
@@ -200,39 +226,39 @@ const getRooms = () => [
       messages: [],
       starCount: 0,
       starredUsers: [],
-      name: "John Doe",
-      username: "john.doe",
-      profilePhoto: "https://avatarmaker.net/images/1.png",
-      userBio: "I'm John Doe, a Sr. Backend Developer",
+      name: 'John Doe',
+      username: 'john.doe',
+      profilePhoto: 'https://avatarmaker.net/images/1.png',
+      userBio: 'I am John Doe, a Sr. Backend Developer',
       activeRoom: null,
-      createdRoom: "60b526e0ec34b4f603d4b64f",
+      createdRoom: '60b526e0ec34b4f603d4b64f',
       waitingRoom: null
     },
-    title: "Cool Room",
-    description: "lorem ipsum dolor sit amet",
-    roomLanguage: "English",
+    title: 'Cool Room',
+    description: 'lorem ipsum dolor sit amet',
+    roomLanguage: 'English',
     maxParticipants: 6,
     canUseMic: true,
     canUseWebcam: true,
     canShareScreen: true,
     canTypeToChatBox: true,
     isPrivate: false,
-    createdAt: "2021-05-31T20:11:44.410Z"
+    createdAt: '2021-05-31T20:11:44.410Z'
   },
   {
-    _id: "60b526e0ec34b4f603d4b71f",
+    _id: '60b526e0ec34b4f603d4b71f',
     participants: [
       {
-        _id: "60b4ce37b66493b3b07ea3e6",
+        _id: '60b4ce37b66493b3b07ea3e6',
         socialLinks: [
-          "https://www.github.com/"
+          'https://www.github.com/'
         ],
         interests: [
-          "Cyber Security"
+          'Cyber Security'
         ],
         spokenLangs: [
-          "English",
-          "Korean"
+          'English',
+          'Korean'
         ],
         friends: [],
         following: [],
@@ -240,32 +266,32 @@ const getRooms = () => [
         messages: [],
         starCount: 0,
         starredUsers: [],
-        name: "Jane Smith",
-        username: "jane.smith",
-        profilePhoto: "https://avatarmaker.net/images/2.png",
-        userBio: "I'm Jane Smith, a Cyber Security Expert",
-        activeRoom: "60b526e0ec34b4f603d4b71f",
-        createdRoom: "60b526e0ec34b4f603d4b64f",
+        name: 'Jane Smith',
+        username: 'jane.smith',
+        profilePhoto: 'https://avatarmaker.net/images/2.png',
+        userBio: 'I am Jane Smith, a Cyber Security Expert',
+        activeRoom: '60b526e0ec34b4f603d4b71f',
+        createdRoom: '60b526e0ec34b4f603d4b64f',
         waitingRoom: null
       }
     ],
     kickedPeople: [],
     waitingPeople: [],
     roomTags: [
-      "#cybersecurity",
-      "#backend"
+      '#cybersecurity',
+      '#backend'
     ],
     owner: {
-      _id: "60b4ce37b66493b3b07ea8e6",
+      _id: '60b4ce37b66493b3b07ea8e6',
       socialLinks: [
-        "https://www.github.com/"
+        'https://www.github.com/'
       ],
       interests: [
-        "Cyber Security"
+        'Cyber Security'
       ],
       spokenLangs: [
-        "English",
-        "Korean"
+        'English',
+        'Korean'
       ],
       friends: [],
       following: [],
@@ -273,25 +299,25 @@ const getRooms = () => [
       messages: [],
       starCount: 0,
       starredUsers: [],
-      name: "Jane Smith",
-      username: "jane.smith",
-      profilePhoto: "https://avatarmaker.net/images/2.png",
-      userBio: "I'm Jane Smith, a Cyber Security Expert",
-      activeRoom: "60b526e0ec34b4f603d4b71f",
-      createdRoom: "60b526e0ec34b4f603d4b64f",
+      name: 'Jane Smith',
+      username: 'jane.smith',
+      profilePhoto: 'https://avatarmaker.net/images/2.png',
+      userBio: 'I am Jane Smith, a Cyber Security Expert',
+      activeRoom: '60b526e0ec34b4f603d4b71f',
+      createdRoom: '60b526e0ec34b4f603d4b64f',
       waitingRoom: null
     },
-    title: "Jane's Room",
-    description: "lorem ipsum dolor sit amet",
-    roomLanguage: "English",
+    title: 'Jane\'s Room',
+    description: 'lorem ipsum dolor sit amet',
+    roomLanguage: 'English',
     maxParticipants: 6,
     canUseMic: true,
     canUseWebcam: true,
     canShareScreen: true,
     canTypeToChatBox: true,
     isPrivate: false,
-    createdAt: "2021-05-31T20:11:44.410Z"
+    createdAt: '2021-05-31T20:11:44.410Z'
   }
 ]
 
-export default getRooms;
+export default getRooms
