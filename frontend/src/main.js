1397ad67d47067702fa792f4eb98d770d5b8087b
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-11-20 21:52:31 +0300

    Add info icon

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,10 +4,10 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faHome, faComment, faCog, faPlusCircle, faEye } from '@fortawesome/free-solid-svg-icons'
+import { faHome, faComment, faCog, faPlusCircle, faEye, faInfoCircle } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faHome, faComment, faCog, faPlusCircle, faEye)
+library.add(faHome, faComment, faCog, faPlusCircle, faEye, faInfoCircle)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
