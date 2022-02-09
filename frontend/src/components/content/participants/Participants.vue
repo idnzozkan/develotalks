d37d19cb6ddd13a88ab187af2771c24e047a34b9
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2022-02-09 23:42:21 +0300

    Create Participants component

---
+++ frontend/src/components/content/participants/Participants.vue
@@ -0,0 +1,53 @@
+<script>
+export default {
+  name: 'Participants'
+}
+</script>
+
+<template lang="pug">
+  .participants-in-room
+    .participant
+    .participant.speaking
+    .participant
+</template>
+
+<style lang="scss">
+  .participants-in-room {
+    flex: 8;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    flex-wrap: wrap;
+    border-radius: 1.25rem;
+    background: rgba(0, 0, 0, 0.2);
+  }
+
+  .participant {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: rgba(131, 64, 40, 0.7);
+    padding: 1rem;
+    border-radius: 1.25rem;
+    height: 40%;
+    width: 40%;
+
+    &::before {
+      content: 'D';
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: white;
+      font-size: 3rem;
+      width: 5vw;
+      height: 5vw;
+      background: rgba(255, 127, 80, 0.7);
+      padding: 1rem;
+      border-radius: 50%;
+    }
+  }
+
+  .speaking {
+    border: 3px solid #6C5DD3;
+  }
+</style>
---
+++ frontend/src/components/content/participants/index.js
@@ -0,0 +1,3 @@
+import Participants from './Participants.vue'
+
+export default Participants
