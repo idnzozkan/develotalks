8491b0e1349d7b8ea565548012244ccd27b5c156
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2022-02-13 23:25:32 +0300

    Use fragment plugin

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import Fragment from 'vue-fragment'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import {
   faHome,
@@ -51,7 +52,7 @@ library.add(
 )
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
-
+Vue.use(Fragment.Plugin)
 Vue.config.productionTip = false
 
 new Vue({
