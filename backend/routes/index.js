1d8569451664398f5e5de3c0d645d13da9f5ebb1
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2022-05-25 02:21:30 +0300

    Separate controllers logic from the routes

--- backend/routes/index.js
+++
@@ -1,8 +0,0 @@
-const { roomsService } = require("../services")
-const router = require("express").Router()
-
-router.get("/ping", async (req, res) => {
-  res.send("pong!")
-})
-
-module.exports = router
