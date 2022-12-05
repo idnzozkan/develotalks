e4ba646afa09ed866f90364c2ea4d35edf36dea8
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2022-12-05 03:59:31 +0300

    Update global room states

--- frontend/src/store/room.js
+++ frontend/src/store/room.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
+
+import { hmsActions } from '../lib/hms'
 import getRooms from '../@fake-db/rooms'
+import ROLES from '../support/constants/roles'
 
 const mutations = {
   SET_JOINED_ROOM: 'SET_JOINED_ROOM',
@@ -10,7 +13,9 @@ const actions = {
   SET_JOINED_ROOM: 'setJoinedRoom',
   FETCH_ROOMS: 'fetchRooms',
   FETCH_FAKE_ROOMS: 'fetchFakeRooms',
-  CREATE_ROOM: 'createRoom'
+  CREATE_ROOM: 'createRoom',
+  JOIN_ROOM: 'joinRoom',
+  LEAVE_ROOM: 'leaveRoom'
 }
 
 const room = {
@@ -35,6 +40,44 @@ const room = {
       const rooms = await getRooms()
       return rooms
     },
+    async [actions.JOIN_ROOM] ({ dispatch, rootState }, { room, role }) {
+      const { _id: userId, name: userName } = rootState.user.user
+      const { _id: roomIdForServer, hmsId: roomIdForHMS } = room
+
+      try {
+        // Server request
+        await axios.post(`/rooms/join?roomId=${roomIdForServer}`)
+
+        const token = await axios.post('/hms/token', { roomId: roomIdForHMS, userId, role })
+        await hmsActions.join({
+          userName,
+          authToken: token.data,
+          settings: {
+            isAudioMuted: false,
+            isVideoMuted: true
+          },
+          metaData: JSON.stringify({ user: rootState.user.user })
+        })
+
+        await dispatch(actions.SET_JOINED_ROOM, room)
+      } catch (err) {
+        await dispatch(actions.LEAVE_ROOM)
+        throw new Error(err)
+      }
+    },
+    async [actions.LEAVE_ROOM] ({ dispatch }) {
+      try {
+        // Server request
+        await axios.post('/rooms/leave')
+
+        // HMS request
+        await hmsActions.leave()
+
+        await dispatch(actions.SET_JOINED_ROOM, null)
+      } catch (err) {
+        console.log(err)
+      }
+    },
     async [actions.CREATE_ROOM] ({ dispatch }, { title, roomTags, roomLanguage, maxParticipants }) {
       const room = await axios.post('/rooms', {
         title,
@@ -42,7 +85,7 @@ const room = {
         roomLanguage,
         maxParticipants
       })
-      dispatch(actions.SET_JOINED_ROOM, room.data)
+      await dispatch(actions.JOIN_ROOM, { room: room.data, role: ROLES.OWNER })
     }
   }
 }
